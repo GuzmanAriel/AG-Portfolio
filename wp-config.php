609e9aa259805a3a863f421cc676e2c724c54517
unknown <guzmanariel525@gmail.com>
2017-12-15 16:29:02 -0500

    update to config

--- wp-config.php
+++ wp-config.php
@@ -46,9 +46,6 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-
-define('WP_HOME', 'http://guzmanster.com');
-define('WP_SITEURL', 'http://guzmanster.com');
 define('AUTH_KEY',         'g.x;!!x>21.~FNuGazdJcu>+el&YdeS t=gaXQY`rwIXTT*JYYPS RaYn1CDnsfv');
 define('SECURE_AUTH_KEY',  '|RZ,S{BBmYnNI)t8D,8 ee Ryfs.H#s#}dfnP%sa0a;j`~J>-=NJjgBwIwnU.MJ0');
 define('LOGGED_IN_KEY',    'FGrUf VNHv9(Z0c6dvN%q-b9#O+7k9w.[9MM(l>W2VT*kjJ9%3rbg}?Yo>y(H2^O');
